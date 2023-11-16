f67f626bae5f8d8ccc044e66912b061a63ffd299
7f23p <133969800+7f23p@users.noreply.github.com>
2023-11-17 00:40:45 +0100

    wip

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,12 +11,6 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
-app.logger.setLevel(logging.INFO)
-log_file = "app.log"
-file_handler = RotatingFileHandler(log_file, maxBytes=10240, backupCount=5)
-formatter = logging.Formatter('%(asctime)s [%(levelname)s] - %(message)s')
-file_handler.setFormatter(formatter)
-app.logger.addHandler(file_handler)
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
