7de7ec412ad1ddcda279c00a1412b452e942af84
7f23p <133969800+7f23p@users.noreply.github.com>
2023-11-17 01:33:30 +0100

    copy

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,16 +7,18 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
+from flask.logging import create_logger
 
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
-app.logger.setLevel(logging.INFO)
-log_file = "app.log"
-file_handler = RotatingFileHandler(log_file, maxBytes=10240, backupCount=5)
-formatter = logging.Formatter('%(asctime)s [%(levelname)s] - %(message)s')
-file_handler.setFormatter(formatter)
-app.logger.addHandler(file_handler)
+# DONE (Note: Instructor's original app.logger code not pylint compliant)
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.INFO)
+LOG.addHandler(streamHandler)
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
